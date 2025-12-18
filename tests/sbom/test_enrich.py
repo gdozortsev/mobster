8e59911f1c910b28243027888662ead73f3f2eae
Gabriela Dozortsev <gdozorts@redhat.com>
2025-12-18 16:03:50 -0500

    generalized workflow to single enrichment function, added functionality for cdx + cdx, cdx + json

--- tests/sbom/test_enrich.py
+++ tests/sbom/test_enrich.py
@@ -49,8 +49,29 @@ async def test_enrich_sboms_cdx_cdx(
     monkeypatch.chdir(data_dir)
     original_sbom_path = data_dir / gemma_cdx
     owasp_sbom_path = data_dir / owasp_gemma_sbom
-
     
     new_sbom = await enrich_sbom(original_sbom_path, owasp_sbom_path)
     with open('enriched_sbom_gemma.json', 'w') as f:
         json.dump(new_sbom, f, indent=2)
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "mock_cdx, mock",
+    [
+        (Path("mock_cdx.json"), Path("mock_enrichment_format.json")),
+    ],
+)
+async def test_enrich_sboms_cdx_json(
+    mock_cdx: Path,
+    mock:Path,
+    data_dir: Path,
+    monkeypatch: pytest.MonkeyPatch,     
+):
+    monkeypatch.chdir(data_dir)
+    original_sbom_path = data_dir / mock_cdx
+    owasp_sbom_path = data_dir / mock
+    
+    new_sbom = await enrich_sbom(original_sbom_path, owasp_sbom_path)
+    with open('enriched_sbom_mock.json', 'w') as f:
+        json.dump(new_sbom, f, indent=2)
+
